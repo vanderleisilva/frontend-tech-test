d8603f2db6fe79ada5971ef33b413f954b614bf1
Vanderlei Alves da Silva <vanderleisilva@santos.sp.gov.br>
2017-10-24 16:10:13 -0200

    Add redux todo basic structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { render } from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import App from './components/App';
+import reducer from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer)
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
+
 registerServiceWorker();
+
